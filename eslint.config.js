bf89a97dc6cbf73f9fbf10f340111eec338eb86a
Jeremías <95102411+JerePrograma@users.noreply.github.com>
2026-07-23 09:30:45 -0300

    fix: use TypeScript-aware unused variable lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import eslint from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import globals from 'globals';
 
@@ -30,6 +31,23 @@ export default [
     },
     rules: {
       'no-console': ['error', { allow: ['warn', 'error'] }],
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
     },
   },
   {
